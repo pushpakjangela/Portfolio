a236eec14b01c3c6554d1be8fad5b86a918047ee
Pushpak Jangela <pushpakjangela@Pushpaks-MacBook-Air.local>
2025-04-26 08:02:55 +0530

    locomotive and other setup is done

---
+++ src/Pages/AboutPage/About.tsx
@@ -0,0 +1,9 @@
+const About = ()=>{
+    return (
+        <div style={{minHeight:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+            <h1>About Me</h1>
+            <p>This is the about page.</p>
+        </div>
+    )
+}
+export default About;
